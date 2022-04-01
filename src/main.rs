4041eb856ea07527fe023d79bf1e463eabacc4ac
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-04-01 16:58:38 -0400

    directory support + safe upload mode

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use axum::body::{self, BoxBody};
-use axum::extract::{Multipart, Path, TypedHeader};
-use axum::headers::{Authorization, authorization::Bearer};
+use axum::extract::{Multipart, Path, Query, TypedHeader};
+use axum::headers::{authorization::Bearer, Authorization};
 use axum::http::{header::CONTENT_TYPE, HeaderValue, StatusCode};
 use axum::response::{Json, Response};
-use axum::routing::{delete, get, post};
+use axum::routing::{get, post};
 use axum::Router;
 
 use tower_http::trace::TraceLayer;
@@ -13,14 +13,27 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use dotenv::dotenv;
 
 use futures::stream::StreamExt;
+use serde::Deserialize;
 use serde_json::{json, Value};
 
 use mime_guess::from_path;
 
 use std::io::ErrorKind::{AlreadyExists, NotFound};
 use std::net::SocketAddr;
+use std::path::Path as StdPath;
 
 use tokio::fs;
+
+#[derive(Deserialize)]
+struct UploadFileQuery {
+    directory: Option<String>,
+    #[serde(default = "default_false")]
+    safe: bool,
+}
+
+fn default_false() -> bool {
+    false
+}
 
 async fn index() -> Json<Value> {
     Json(json!({
@@ -30,16 +43,41 @@ async fn index() -> Json<Value> {
 
 async fn upload_file(
     TypedHeader(authorization): TypedHeader<Authorization<Bearer>>,
+    Query(UploadFileQuery { directory, safe }): Query<UploadFileQuery>,
     mut multipart: Multipart,
 ) -> Result<Json<Value>, StatusCode> {
-    if authorization.token() != std::env::var("AUTH_TOKEN").unwrap_or_else(|_| "aaa".to_string()).as_str() {
+    if authorization.token()
+        != std::env::var("AUTH_TOKEN")
+            .unwrap_or_else(|_| "aaa".to_string())
+            .as_str()
+    {
         return Err(StatusCode::UNAUTHORIZED);
     }
 
     if let Ok(Some(mut field)) = multipart.next_field().await {
         if field.file_name().is_some() {
-            let filename = field.file_name().unwrap_or_else(|| unreachable!()).to_string();
-            let path = format!("/home/services/cdn/uploads/{}", filename);
+            let filename = field
+                .file_name()
+                .unwrap_or_else(|| unreachable!())
+                .to_string();
+            let directory = directory
+                .map(|d| {
+                    if d == "/" {
+                        return Ok(d);
+                    }
+                    if d.contains("/") {
+                        return Err(StatusCode::BAD_REQUEST);
+                    }
+                    Ok(format!("/{}/", d.trim_matches('/')))
+                })
+                .unwrap_or_else(|| Ok("/".to_string()))?;
+
+            let base_path = format!("{}{}", directory, filename);
+            let path_string = format!("/home/services/cdn/uploads{}", base_path);
+            let path = StdPath::new(&path_string);
+            if safe && path.exists() {
+                return Err(StatusCode::CONFLICT);
+            }
 
             let mut buffer: Vec<u8> = Vec::new();
             let mut file_size: u64 = 0;
@@ -61,7 +99,10 @@ async fn upload_file(
                 .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
             return Ok(Json(json!({
-                "message": "File uploaded"
+                "message": "File uploaded",
+                "filename": filename,
+                "directory": directory,
+                "path": base_path,
             })));
         } else {
             return Err(StatusCode::BAD_REQUEST);
@@ -71,15 +112,11 @@ async fn upload_file(
     Err(StatusCode::BAD_REQUEST)
 }
 
-async fn get_file(Path(filename): Path<String>) -> Result<Response<BoxBody>, StatusCode> {
-    let path = format!("/home/services/cdn/uploads/{}", filename);
-
-    let file = fs::read(path)
-        .await
-        .map_err(|e| match e.kind() {
-            NotFound => StatusCode::NOT_FOUND,
-            _ => StatusCode::INTERNAL_SERVER_ERROR
-        })?;
+async fn handle_get_file(filename: String, path: String) -> Result<Response<BoxBody>, StatusCode> {
+    let file = fs::read(path).await.map_err(|e| match e.kind() {
+        NotFound => StatusCode::NOT_FOUND,
+        _ => StatusCode::INTERNAL_SERVER_ERROR,
+    })?;
 
     let resp = Response::builder()
         .status(StatusCode::OK)
@@ -90,27 +127,52 @@ async fn get_file(Path(filename): Path<String>) -> Result<Response<BoxBody>, Sta
                     .first_or_octet_stream()
                     .to_string()
                     .as_str(),
-            ).unwrap_or_else(|_| HeaderValue::from_static("application/octet-stream")),
+            )
+            .unwrap_or_else(|_| HeaderValue::from_static("application/octet-stream")),
         )
         .body(body::boxed(body::Full::from(file)))
-        .unwrap_or_else(|e| unreachable!("{e:?}"));
+        .unwrap_or_else(|_e| unreachable!("{_e:?}"));
 
     Ok(resp)
+}
+
+async fn get_file(Path(filename): Path<String>) -> Result<Response<BoxBody>, StatusCode> {
+    let path = format!("/home/services/cdn/uploads/{}", filename);
+
+    handle_get_file(filename, path).await
+}
+
+async fn get_file_with_dir(
+    Path((dir, filename)): Path<(String, String)>,
+) -> Result<Response<BoxBody>, StatusCode> {
+    let path = format!("/home/services/cdn/uploads/{}/{}", dir, filename);
+
+    handle_get_file(filename, path).await
+}
+
+async fn handle_delete_file(path: String) -> Result<Json<Value>, StatusCode> {
+    fs::remove_file(path).await.map_err(|e| match e.kind() {
+        NotFound => StatusCode::NOT_FOUND,
+        _ => StatusCode::INTERNAL_SERVER_ERROR,
+    })?;
+
+    Ok(Json(json!({
+        "message": "File deleted"
+    })))
 }
 
 async fn delete_file(Path(filename): Path<String>) -> Result<Json<Value>, StatusCode> {
     let path = format!("/home/services/cdn/uploads/{}", filename);
 
-    fs::remove_file(path)
-        .await
-        .map_err(|e| match e.kind() {
-            NotFound => StatusCode::NOT_FOUND,
-            _ => StatusCode::INTERNAL_SERVER_ERROR
-        })?;
+    handle_delete_file(path).await
+}
 
-    Ok(Json(json!({
-        "message": "File deleted"
-    })))
+async fn delete_file_with_dir(
+    Path((dir, filename)): Path<(String, String)>,
+) -> Result<Json<Value>, StatusCode> {
+    let path = format!("/home/services/cdn/uploads/{}/{}", dir, filename);
+
+    handle_delete_file(path).await
 }
 
 #[tokio::main]
@@ -118,16 +180,15 @@ async fn main() {
     dotenv().ok();
 
     tracing_subscriber::registry()
-    .with(tracing_subscriber::EnvFilter::new(
-        std::env::var("RUST_LOG")
-            .unwrap_or_else(|_| "tower_http=info".into()),
-    ))
-    .with(tracing_subscriber::fmt::layer())
-    .init();
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "tower_http=info".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-
-    let _ =
-        fs::create_dir_all("/home/services/cdn/uploads".to_string()).await.map_err(|e| match e.kind() {
+    let _ = fs::create_dir_all("/home/services/cdn/uploads".to_string())
+        .await
+        .map_err(|e| match e.kind() {
             AlreadyExists => (),
             _ => panic!("{e:?}"),
         });
@@ -135,8 +196,11 @@ async fn main() {
     let router = Router::new()
         .route("/", get(index))
         .route("/upload", post(upload_file))
-        .route("/uploads/:filename", get(get_file))
-        .route("/uploads/:filename", delete(delete_file))
+        .route(
+            "/uploads/:dir/:filename",
+            get(get_file_with_dir).delete(delete_file_with_dir),
+        )
+        .route("/uploads/:filename", get(get_file).delete(delete_file))
         .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8083));
