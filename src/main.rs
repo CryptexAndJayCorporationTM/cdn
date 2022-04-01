24628e0567f66ec630ab2d704d5426b0eef3a80c
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-03-31 17:31:42 -0700

    aaa

--- src/main.rs
+++ src/main.rs
@@ -120,7 +120,7 @@ async fn main() {
     tracing_subscriber::registry()
     .with(tracing_subscriber::EnvFilter::new(
         std::env::var("RUST_LOG")
-            .unwrap_or_else(|_| "tower_http=info".into()),
+            .unwrap_or_else(|_| "tower_http=debug".into()),
     ))
     .with(tracing_subscriber::fmt::layer())
     .init();
