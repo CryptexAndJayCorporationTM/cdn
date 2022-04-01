65d4e40a620534137c16e6cae8c7deb4af22b7f4
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-03-31 17:38:46 -0700

    ok default to info is fine

--- src/main.rs
+++ src/main.rs
@@ -120,7 +120,7 @@ async fn main() {
     tracing_subscriber::registry()
     .with(tracing_subscriber::EnvFilter::new(
         std::env::var("RUST_LOG")
-            .unwrap_or_else(|_| "tower_http=debug".into()),
+            .unwrap_or_else(|_| "tower_http=info".into()),
     ))
     .with(tracing_subscriber::fmt::layer())
     .init();
