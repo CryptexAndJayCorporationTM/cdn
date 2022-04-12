04d88ce5cfd8532ef9f630d90a14eb149cc7551f
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-04-11 22:56:31 -0400

    S, paste html functionality

--- src/main.rs
+++ src/main.rs
@@ -146,6 +146,48 @@ async fn get_file(Path(filename): Path<String>) -> Result<Response<BoxBody>, Sta
     handle_get_file(filename, path).await
 }
 
+async fn get_paste(Path(filename): Path<String>) -> Result<Response<BoxBody>, StatusCode> {
+    let path = format!("/home/services/cdn/uploads/{}", filename);
+    let text = match String::from_utf8(fs::read(path).await.map_err(|e| match e.kind() {
+        NotFound => StatusCode::NOT_FOUND,
+        _ => StatusCode::INTERNAL_SERVER_ERROR,
+    })?) {
+        Ok(s) => s,
+        Err(_) => {
+            return Err(StatusCode::NOT_FOUND);
+        }
+    };
+    let sanitized = filename.trim_matches('/').split('/').last().unwrap();
+
+    let html = format!(
+        r#"
+            <html><head><link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/atom-one-dark.min.css">
+            <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js"></script><style>@import url(
+            'https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
+            ); *{{font-family: 'Jetbrains Mono', monospace;}}body{{margin: 0; background: #282c34;}}pre{{margin: 0; padding: 16px; font-size: 14px; min-height: 100%;}}div
+            {{display: flex; align-items: center;}}div div{{display: flex; align-items: center; margin-left: 16px; flex-grow: 1; padding-top: 12px;}}button
+            {{border: none; cursor: pointer; color: white; background-color: #14a84c; border-radius: 4px; font-size: 14px; margin: 6px; padding: 8px; min-width: 64px;
+            opacity: 0.75; transition: all 0.3s ease;}}button:hover{{opacity: 1; background-color: #17bd56;}}h2{{margin: 16px 0 0 16px; font-weight: 400; 
+            color: rgba(255, 255, 255, 0.3); font-size: 1.2em;}}h2, button{{-webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none;}}
+            h2 #second{{color: rgba(255, 255, 255, 0.33); font-weight: 500;}}</style></head><body><div> <h2> <span id="first">Paste: </span> <span id="second"><i>{}</i></span>
+            </h2> <div><button onclick="copyButton()" id="copy">Copy</button> <button onclick="copyLink()" id="copylink">Copy Link</button></div></div>
+            <pre style="word-wrap: break-word; white-space: pre-wrap;" id="o">{}</pre><script>document.querySelectorAll('pre').forEach(el=>{{hljs.highlightElement(el);}});
+            function copy(text){{navigator.clipboard.writeText(text);}}function copyButton(){{copy(document.getElementById('o').innerText); let el=document.getElementById('copy');
+            el.innerHTML='Copied!'; setTimeout(()=>{{el.innerHTML='Copy';}}, 1000);}}function copyLink(){{copy(window.location.href); let el=document.getElementById('copylink');
+            el.innerHTML='Copied Link!'; setTimeout(()=>{{el.innerHTML='Copy Link';}},1000);}}</script> </body></html>
+        "#,
+        sanitized, text,
+    );
+
+    let resp = Response::builder()
+        .status(StatusCode::OK)
+        .header(CONTENT_TYPE, HeaderValue::from_static("text/html"))
+        .body(body::boxed(body::Full::from(html)))
+        .unwrap_or_else(|_e| unreachable!("{_e:?}"));
+
+    Ok(resp)
+}
+
 async fn handle_delete_file(path: String) -> Result<Json<Value>, StatusCode> {
     fs::remove_file(path).await.map_err(|e| match e.kind() {
         NotFound => StatusCode::NOT_FOUND,
@@ -185,6 +227,7 @@ async fn main() {
         .route("/", get(index))
         .route("/upload", post(upload_file))
         .route("/uploads/*filename", get(get_file).delete(delete_file))
+        .route("/pastes/*filename", get(get_paste))
         .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8083));
